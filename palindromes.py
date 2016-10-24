a27304f0d6078e95a306904c74dea286875fa794
Leeestephen <leestephen75@gmail.com>
2016-10-24 16:04:19 -0400

    Add palindromes draft

---
+++ palindromes.py
@@ -0,0 +1,55 @@
+def is_palindrome():
+	"""(str) -> bool
+
+	The parameter is a string consisting only of lowercase alphabetic letters
+	that may or may not be a palindrome. Return True if and only if the
+	parameter is a palindrome.
+
+	>>> is_palindrome()
+
+	>>> is_palindrome()
+
+	>>> is_palindrome()
+
+	"""
+
+	# body
+
+
+def is_palindromic_phrase(): 
+	"""(str) -> bool
+
+	The parameter is a string that may be a palindrome. Return True if and only
+	if the parameter is a palindrome, ignoring case and non-alphabetic
+	characters. (To be clear, non-alphabetic characters should be ignored as if
+	they are not present, and uppercase letters should be considered to be
+	equal to their lowercase equivalents.)
+
+	>>> is_palindromic_phrase()
+
+	>>> is_palindromic_phrase()
+
+	>>> is_palindromic_phrase()
+
+	"""
+
+	# body
+
+
+def get_odd_palindrome_at():
+	"""(str, int) -> str
+
+	The first parameter is a string consisting only of lowercase alphabetic
+	characters, and the second parameter is a valid index into the string.
+	Return the longest odd-length palindrome in the string that is centered at
+	the specified index.
+
+	>>> get_odd_palindrome_at()
+
+	>>> get_odd_palindrome_at()
+
+	>>> get_odd_palindrome_at()
+
+	"""
+
+	# body
